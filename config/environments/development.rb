5222925a5534d1bbfa89abc78c91279eab5b2f59
crateros <dsvene@gmail.com>
2016-11-16 16:26:08 -0800

    Add Devise for User model and authentication, create routes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Necessary for Devise gem, should be set to actual URL for production.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
